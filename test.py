8ad8243d24826341ea85eb1d0615bb71d0d1188a
unknown <blogerxuuz@gmail.com>
2025-10-06 11:29:47 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+hafta_kunlari = [
+    "Yakshanba", 
+    "Dushanba", 
+    "Seshanba", 
+    "Chorshanba", 
+    "Payshanba", 
+    "Juma", 
+    "Shanba"
+]
+K = int(input("K kunini kiriting (1-365 oralig'ida): "))
+
+kun_index = (1 + (K - 1)) % 7
+
+print(f"{K}-kun haftaning {hafta_kunlari[kun_index]} kuniga to‘g‘ri keladi.")
+print("Dastur tugadi.")
